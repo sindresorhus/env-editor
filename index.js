7c81ca807cc2e6db2db4b9f2c67756d14ca8e794
Sindre Sorhus <sindresorhus@gmail.com>
2017-05-04 21:56:17 +0800

    Init

---
+++ index.js
@@ -0,0 +1,98 @@
+'use strict';
+const path = require('path');
+
+const editors = () => [{
+	id: 'sublime',
+	name: 'Sublime Text',
+	bin: 'subl',
+	paths: [
+		'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl',
+		'/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl'
+	],
+	keywords: []
+}, {
+	id: 'atom',
+	name: 'Atom',
+	bin: 'atom',
+	paths: [
+		'/Applications/Atom.app/Contents/Resources/app/atom.sh'
+	],
+	keywords: []
+}, {
+	id: 'vscode',
+	name: 'Visual Studio Code',
+	bin: 'code',
+	paths: [
+		'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code'
+	],
+	keywords: [
+		'vs code'
+	]
+}, {
+	id: 'webstorm',
+	name: 'WebStorm',
+	bin: 'wstorm',
+	paths: [],
+	keywords: []
+}, {
+	id: 'textmate',
+	name: 'TextMate',
+	bin: 'mate',
+	paths: [],
+	keywords: []
+}];
+
+const get = input => {
+	input = input.toLowerCase().trim();
+	const bin = input.split(path.sep).pop();
+
+	for (const editor of editors()) {
+		// TODO: Maybe use `leven` module for more flexible matching
+		if (
+			input === editor.id ||
+			input === editor.name.toLowerCase() ||
+			bin === editor.bin
+		) {
+			return editor;
+		}
+
+		for (const p of editor.paths) {
+			if (path.normalize(input) === path.normalize(p)) {
+				return editor;
+			}
+		}
+
+		for (const keyword of editor.keywords) {
+			if (input === keyword) {
+				return editor;
+			}
+		}
+	}
+
+	return {
+		id: input,
+		name: input,
+		bin: input,
+		paths: []
+	};
+};
+
+module.exports.default = () => {
+	const editor = process.env.EDITOR || process.env.VISUAL;
+
+	if (!editor) {
+		throw new Error(
+`
+Your $EDITOR environment variable is not set.
+Set it to the command/path of your editor in ~/.zshenv or ~/.bashrc:
+
+  export EDITOR=atom
+`
+		);
+	}
+
+	return get(editor);
+};
+
+module.exports.get = get;
+module.exports.all = editors;
---
+++ test.js
@@ -0,0 +1,48 @@
+import test from 'ava';
+import m from '.';
+
+test.serial('.default()', t => {
+	const editor = process.env.EDITOR;
+	process.env.EDITOR = 'subl';
+	t.is(m.default().id, 'sublime');
+	process.env.EDITOR = editor;
+});
+
+test('.get() - Sublime', t => {
+	t.is(m.get('sublime').id, 'sublime');
+	t.is(m.get('Sublime').id, 'sublime');
+	t.is(m.get('subl').id, 'sublime');
+	t.is(m.get('Sublime Text').id, 'sublime');
+	t.is(m.get('/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl').id, 'sublime');
+});
+
+test('.get() - Atom', t => {
+	t.is(m.get('atom').id, 'atom');
+	t.is(m.get('Atom').id, 'atom');
+});
+
+test('.get() - Visual Studio Code', t => {
+	t.is(m.get('vscode').id, 'vscode');
+	t.is(m.get('code').id, 'vscode');
+	t.is(m.get('vs code').id, 'vscode');
+	t.is(m.get('Visual Studio Code').id, 'vscode');
+});
+
+test('.get() - WebStorm', t => {
+	t.is(m.get('webstorm').id, 'webstorm');
+	t.is(m.get('wstorm').id, 'webstorm');
+	t.is(m.get('WebStorm').id, 'webstorm');
+});
+
+test('.get() - TextMate', t => {
+	t.is(m.get('textmate').id, 'textmate');
+	t.is(m.get('mate').id, 'textmate');
+	t.is(m.get('TextMate').id, 'textmate');
+});
+
+test('.all()', t => {
+	const all = m.all();
+	t.true(Array.isArray(all));
+	t.true(all.length > 0);
+	t.is(all[0].id, 'sublime');
+});
